7ca36da5b2fdd0b34bacfde69f748bd73e9163f6
Raphael Parreira <raphaelbp12@gmail.com>
2023-06-26 12:47:20 +0200

    feat: change presentation text

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -11,12 +11,19 @@ const Presentation: React.FC = () => (
       className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-full mb-4 md:mb-0 md:mr-8"
     />
     <div>
-      <h1 className="text-2xl font-bold mb-4">Hello, I'm Raphael</h1>
+      <h1 className="text-2xl font-bold mb-4">Hello!</h1>
       <p className="text-lg leading-relaxed">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        I'm Raphael, a Software Developer based in Copenhagen. My passion is the
+        process of developing new products, no matter what. My current side
+        project is an{" "}
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://www.sacerdosidle.com/"
+        >
+          Idle Game
+        </a>
+        .
       </p>
     </div>
   </section>
