eb4bb8043e31badbe6eff0a85850e2238a988ea5
Raphael Parreira <raphaelbp12@gmail.com>
2023-06-26 12:49:14 +0200

    feat: add new projects and change project descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,28 @@ const App = () => (
       <Presentation />
       <section id="projects">
         <Project
+          title="Sacerdos Idle Game"
+          subtitle="Stack: ReactJS, Tailwind, Typescript, .Net Core, PostgreSQL"
+          description="This app was a fun activity to explore more about the ChatGPT power. In this project I acted as a Product Owner, and had the GPT as the programmer. It worked flawlessly, a great experience. This is an open-source project, anyone can copy and modify it."
+          imageUrl="/sacerdos.png"
+          liveUrl="https://www.sacerdosidle.com/"
+        />
+        <Project
+          title="This Portfolio"
+          subtitle="Stack: ReactJS, Tailwind, Typescript"
+          description="This app was a fun activity to explore more about the ChatGPT power. In this project I acted as a Product Owner, and had the GPT as the programmer. It worked flawlessly, a great experience. This is an open-source project, anyone can copy and modify it."
+          sourceUrl="https://github.com/raphaelbp12/portfolio"
+        />
+        <Project
           title="Awedash"
           subtitle="Stack: VueJS, NodeJS, MongoDB"
-          description="Awedash started to be developed at 1st of April of 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          description="Awedash started to be developed on April of 2019. This project has an heavy usage of external APIs (Youtube, Twitter, Instagram, Facebook etc). Since it is not live anymore, I made a quick video that you can check what it was about."
           videoUrl="https://www.youtube.com/embed/EmjznB9xuo4"
         />
         <Project
           title="Skinsmine"
           subtitle="Stack: Ruby on Rails, jQuery, PostgreSQL"
-          description="Skinsmine is a project developed in 2015 with a friend. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          description="Skinsmine is a project developed in 2015 with a friend. It was a Counter-Strike betting website, where you could bet items earned in game. For that, it had a heavy usage of Steam API. You can watch the video where I show the project."
           videoUrl="https://www.youtube.com/embed/lxydKaaWu4o"
         />
         {/* Add as many Project components as you have projects, each with their own description and videoUrl props. */}
